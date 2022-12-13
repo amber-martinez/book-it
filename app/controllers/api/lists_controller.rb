2a278d4b6dcaca7dcdc283e85f3517852ee4386e
Amber <martinez.amber1028@gmail.com>
2022-12-13 12:26:55 -0500

    Add lists controller methods

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,2 +1,31 @@
 class Api::ListsController < ApplicationController
+
+    def show
+        list = List.find(params[:id])
+        render json: list
+    end
+
+    def create
+        list = List.create!(list_params)
+        render json: list, status: :created
+    end
+
+    def update
+        list = List.find(params[:id])
+        list.update!(list_params)
+        render json: list
+    end
+
+    def destroy
+        list = List.find(params[:id])
+        list.destroy
+        head :no_content
+    end
+
+    private
+
+    def list_params
+        params.permit(:title, :user_id, :theme_id)
+    end
+
 end
