3521137eeacff270dcd65e0a6999ce91fade5daf
Amber <martinez.amber1028@gmail.com>
2022-12-13 12:20:14 -0500

    Add user controller methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,31 @@
 class Api::UsersController < ApplicationController
+    skip_before_action :create
+
+    def show
+        render json: @user
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def update
+        @user.update!(user_params)
+        render json: @user
+    end
+
+    def destroy
+        @user.destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :username, :password, :password_confirmation)
+    end
+
 end
