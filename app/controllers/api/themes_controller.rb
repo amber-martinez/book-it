c54068ffa29e6a3388b62ba7721c41dcb1385cef
Amber <martinez.amber1028@gmail.com>
2022-12-13 12:14:35 -0500

    Add application controller methods

---
+++ app/controllers/api/themes_controller.rb
@@ -0,0 +1,2 @@
+class Api::ThemesController < ApplicationController
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  before_action :authorize
+
+  def authorize 
+    @user = User.find(session[:user_id])
+    render json: { error: 'Wrong credentials.' }, status: :unauthorized unless @user
+  end
+
+
+  private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: 'Not found!' }, status: :not_found
+  end
+
+
 end
