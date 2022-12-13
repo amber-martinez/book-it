73a95a9ecba5f6f89797721d6102bb730146beac
Amber <martinez.amber1028@gmail.com>
2022-12-13 12:23:32 -0500

    Add themes controller methods

--- app/controllers/api/themes_controller.rb
+++ app/controllers/api/themes_controller.rb
@@ -1,2 +1,32 @@
 class Api::ThemesController < ApplicationController
+
+    def show
+        theme = Theme.find(params[:id])
+        render json: theme
+    end
+
+    def create
+        theme = Theme.create!(theme_params)
+        render json: theme, status: :created
+    end
+
+    def updated
+        theme = Theme.find(params[:id])
+        theme.update!(theme_params)
+        render json: theme
+    end
+
+    def destroy
+        theme = Theme.find(params[:id])
+        theme.destroy
+        head :no_content
+    end
+
+
+    private
+
+    def theme_params
+        params.permit(:prim_color, :sec_color, :bullet_icon)
+    end
+
 end
