5b0b0dc95ed55505ef9c42d464179a6404bfe520
Amber <martinez.amber1028@gmail.com>
2022-12-13 12:30:36 -0500

    Add bookmarks controller methods

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -1,2 +1,36 @@
 class Api::BookmarksController < ApplicationController
+
+    def index
+        bookmarks = Bookmark.all
+        render json: bookmarks
+    end
+
+    def show
+        bookmark = Bookmark.find(params[:id])
+        render json: bookmark
+    end
+
+    def create 
+        bookmark = Bookmark.create!(bookmark_params)
+        render json: bookmark
+    end
+
+    def update 
+        bookmark = Bookmark.find(params[:id])
+        bookmark.update!(bookmark_params)
+        render json: bookmark
+    end
+    
+    def destroy
+        bookmark = Bookmark.find(params[:id])
+        bookmark.destroy
+        head :no_content
+    end
+
+    private
+
+    def bookmark_params
+        params.permit(:name, :link, :resolved, :user_id, :list_id)
+    end
+
 end
