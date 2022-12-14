9026ce3902a207d59aa2b27ccb4d27e1ddfd963f
Amber <martinez.amber1028@gmail.com>
2022-12-14 13:20:47 -0500

    Add minor styling changes for new theme component

--- client/src/CreateNew/NewListTheme.js
+++ client/src/CreateNew/NewListTheme.js
@@ -34,8 +34,8 @@ function NewListTheme({ primColor, setPrimColor, secColor, setSecColor, bulletIc
 
     return (
         <div style={{ display: 'inline-block'}}>
-            <h6>Theme</h6>
-            <Row style={{ marginBottom: 5, textAlign: 'left', marginTop: 25 }}>
+            <h6 style={{ fontSize: 17, textAlign: 'left' }}>Theme</h6>
+            <Row style={{ marginBottom: 5, textAlign: 'left', marginTop: 17, fontSize: 13 }}>
                 <Col>
                     <p>Primary color</p>
                 </Col>
@@ -43,7 +43,7 @@ function NewListTheme({ primColor, setPrimColor, secColor, setSecColor, bulletIc
                     <input onChange={((e) => setPrimColor(e.target.value))} type="color" class="form-control form-control-color" id="exampleColorInput" value={primColor} title="Choose your color" style={{ border: 'none', height: 34, marginTop: -5 }}></input>
                 </Col>
             </Row>
-            <Row style={{ marginBottom: 5, textAlign: 'left' }}>
+            <Row style={{ marginBottom: 5, textAlign: 'left', fontSize: 13 }}>
                 <Col>
                     <p>Secondary color</p>
                 </Col>
@@ -51,15 +51,15 @@ function NewListTheme({ primColor, setPrimColor, secColor, setSecColor, bulletIc
                     <input onChange={((e) => setSecColor(e.target.value))} type="color" class="form-control form-control-color" id="exampleColorInput" value={secColor} title="Choose your color" style={{ border: 'none', height: 34, marginTop: -5 }}></input>
                 </Col>
             </Row>
-            <Row style={{ marginBottom: 5, textAlign: 'left' }}>
+            <Row style={{ marginBottom: 5, textAlign: 'left', fontSize: 13 }}>
                 <Col>
                     <p>Bullet icon</p>
                 </Col>
                 <Col>
                     <input onChange={((e) => setBulletIcon(e.target.value))} type='text' value={bulletIcon} style={{ width: 48, border: 'none', borderBottom: '.8px solid #4f564e', fontSize: 13, textAlign: 'center', color: '#4f564e' }}></input>
                 </Col>
-                <div style={{ textAlign: 'center' }}>
-                    <button  onClick={onThemeSave} style={{ backgroundColor: '#7e857d', border: 'solid 1px #7e857d', borderColor: '#657065d9', textAlign: 'center', fontSize: 12, marginTop: 10, display: 'inline-block', marginLeft: 10, borderRadius: 3, color: 'white', padding: '2px 10px 3px 10px' }}>Save edits</button>
+                <div style={{ textAlign: 'left', marginTop: 10 }}>
+                    <button  onClick={onThemeSave} style={{ backgroundColor: '#7e857d', border: 'solid 1px #7e857d', borderColor: '#657065d9', textAlign: 'center', fontSize: 12, marginTop: 10, display: 'inline-block', borderRadius: 3, color: 'white', padding: '2px 10px 3px 10px' }}>Save edits</button>
                 </div>
                 <div style={{ marginTop: 15 }}>
                     {errors ? errors.map(e => <p style={{ marginBottom: 3 }}>{e}</p>) : null}
