18cdaa52caeb232a68f036b0e8ab1029041b7843
Amber <martinez.amber1028@gmail.com>
2022-12-14 13:01:53 -0500

    Add styling for new theme

--- client/src/CreateNew/NewListTheme.js
+++ client/src/CreateNew/NewListTheme.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import EditTheme from '../EditElements/EditTheme';
 
 function NewListTheme({ primColor, setPrimColor, secColor, setSecColor, bulletIcon, setBulletIcon }) {
 
@@ -30,12 +33,39 @@ function NewListTheme({ primColor, setPrimColor, secColor, setSecColor, bulletIc
     }
 
     return (
-        <div>
+        <div style={{ display: 'inline-block'}}>
             <h6>Theme</h6>
-            Primary Color: <input type='color' value={primColor} onChange={((e) => setPrimColor(e.target.value))}></input><br></br>
-            Secondary Color: <input type='color' value={secColor} onChange={((e) => setSecColor(e.target.value))}></input><br></br>
-            Bullet Icon: <input type='text' value={bulletIcon} onChange={((e) => setBulletIcon(e.target.value))}></input><br></br>
-            <button onClick={onThemeSave}>Save theme</button>
+            <Row style={{ marginBottom: 5, textAlign: 'left', marginTop: 25 }}>
+                <Col>
+                    <p>Primary color</p>
+                </Col>
+                <Col>
+                    <input onChange={((e) => setPrimColor(e.target.value))} type="color" class="form-control form-control-color" id="exampleColorInput" value={primColor} title="Choose your color" style={{ border: 'none', height: 34, marginTop: -5 }}></input>
+                </Col>
+            </Row>
+            <Row style={{ marginBottom: 5, textAlign: 'left' }}>
+                <Col>
+                    <p>Secondary color</p>
+                </Col>
+                <Col>
+                    <input onChange={((e) => setSecColor(e.target.value))} type="color" class="form-control form-control-color" id="exampleColorInput" value={secColor} title="Choose your color" style={{ border: 'none', height: 34, marginTop: -5 }}></input>
+                </Col>
+            </Row>
+            <Row style={{ marginBottom: 5, textAlign: 'left' }}>
+                <Col>
+                    <p>Bullet icon</p>
+                </Col>
+                <Col>
+                    <input onChange={((e) => setBulletIcon(e.target.value))} type='text' value={bulletIcon} style={{ width: 48, border: 'none', borderBottom: '.8px solid #4f564e', fontSize: 13, textAlign: 'center', color: '#4f564e' }}></input>
+                </Col>
+                <div style={{ textAlign: 'center' }}>
+                    <button  onClick={onThemeSave} style={{ backgroundColor: '#7e857d', border: 'solid 1px #7e857d', borderColor: '#657065d9', textAlign: 'center', fontSize: 12, marginTop: 10, display: 'inline-block', marginLeft: 10, borderRadius: 3, color: 'white', padding: '2px 10px 3px 10px' }}>Save edits</button>
+                </div>
+                <div style={{ marginTop: 15 }}>
+                    {errors ? errors.map(e => <p style={{ marginBottom: 3 }}>{e}</p>) : null}
+                    {successMessage ? <p>Theme saved.</p> : null }
+                </div>
+            </Row>
             {successMessage ? <p>New theme saved!</p> : null}
             {errors ? errors.map(e => <p>{e}</p>) : null}
         </div>
