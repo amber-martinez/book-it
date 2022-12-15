5bbb1b22183f956f9333cbe395333f036d76164c
Amber <martinez.amber1028@gmail.com>
2022-12-15 16:54:32 -0500

    Add finished features page

--- client/src/CoreComponents/Features.js
+++ client/src/CoreComponents/Features.js
@@ -38,13 +38,27 @@ function Features() {
             </Row>
             <Row className="justify-content-md-center" style={{ marginTop: 70, marginBottom: 90 }}>
                 <Col style={{ marginRight: 30 }} xs lg="3" className='hidden' id='featuresTwo'>
-                    <img src='https://i.imgur.com/4EREFCM.gif' alt='customize theme' style={{ height: 260 }}></img>
+                    <img src='https://i.imgur.com/Q5Sflw3.gif' alt='customize theme' style={{ height: 260 }}></img>
                 </Col>
                 <Col style={{ marginLeft: 30, textAlign: 'left', marginTop: 25 }} xs lg="2" className='hidden' id='featuresTwo'>
-                    <h3 style={{ fontSize: 16, marginBottom: 12 }}>Make a list</h3>
-                    <p style={{ fontSize: 13, marginBottom: 12 }}>Create lists to sort your bookmarks by group.</p>
-                    <p style={{ fontSize: 13 }}>Have a running list of the books you want to buy and/or read? Set them here.</p>
-                    <p style={{ fontSize: 13 }}>Name these lists anything you want.</p>
+                    <h3 style={{ fontSize: 16, marginBottom: 12 }}>Choose a theme</h3>
+                    <p style={{ fontSize: 13, marginBottom: 12 }}>Organize your lists by color.</p>
+                    <p style={{ fontSize: 13 }}>Choose an existing theme from our libary.</p>
+                    <p style={{ fontSize: 13 }}>Or...</p>
+                </Col>
+                <div>
+                    <img src='https://i.imgur.com/GaXfsYx.png' style={{ scale: '15%' }}></img>
+                </div>
+            </Row>
+            <Row className="justify-content-md-center" style={{ marginTop: 70, marginBottom: 90 }}>
+                <Col style={{ marginRight: 30 }} xs lg="3" className='hidden' id='featuresTwo'>
+                    <img src='https://i.imgur.com/bAIVjQf.gif' alt='customize theme' style={{ height: 260 }}></img>
+                </Col>
+                <Col style={{ marginLeft: 30, textAlign: 'left', marginTop: 25 }} xs lg="2" className='hidden' id='featuresTwo'>
+                    <h3 style={{ fontSize: 16, marginBottom: 12 }}>Create a theme</h3>
+                    <p style={{ fontSize: 13, marginBottom: 12 }}>Customize your own theme.</p>
+                    <p style={{ fontSize: 13 }}>Choose a primary and secondary color.</p>
+                    <p style={{ fontSize: 13 }}>Top it off with a custom bullet point for each bookmark.</p>
                 </Col>
                 <div>
                     <img src='https://i.imgur.com/GaXfsYx.png' style={{ scale: '15%' }}></img>
@@ -52,13 +66,13 @@ function Features() {
             </Row>
             <Row className="justify-content-md-center" style={{ marginTop: 70, marginBottom: 370 }}>
                 <Col style={{ marginRight: 30 }} xs lg="3" className='hidden' id='featuresTwo'>
-                    <img src='https://i.imgur.com/4EREFCM.gif' alt='customize theme' style={{ height: 260 }}></img>
+                    <img src='https://i.imgur.com/ZYvoOkw.gif' alt='customize theme' style={{ height: 300 }}></img>
                 </Col>
                 <Col style={{ marginLeft: 30, textAlign: 'left', marginTop: 25 }} xs lg="2" className='hidden' id='featuresTwo'>
-                    <h3 style={{ fontSize: 16, marginBottom: 12 }}>Make a list</h3>
-                    <p style={{ fontSize: 13, marginBottom: 12 }}>Create lists to sort your bookmarks by group.</p>
-                    <p style={{ fontSize: 13 }}>Have a running list of the books you want to buy and/or read? Set them here.</p>
-                    <p style={{ fontSize: 13 }}>Name these lists anything you want.</p>
+                    <h3 style={{ fontSize: 16, marginBottom: 12 }}>Access your bookmarks</h3>
+                    <p style={{ fontSize: 13, marginBottom: 12 }}>No need to remember which site you saved your bookmark on.</p>
+                    <p style={{ fontSize: 13 }}>Customize your lists and edit bookmarks at any time.</p>
+                    <p style={{ fontSize: 13 }}>All your bookmarks–one place.</p>
                 </Col>
             </Row>
         </div>
