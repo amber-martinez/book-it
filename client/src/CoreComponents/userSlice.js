fe3c208429eda6b033c1e3ac662f5a5367b71110
Amber <martinez.amber1028@gmail.com>
2022-12-19 16:53:48 -0500

    Set initial account state to null

--- client/src/CoreComponents/userSlice.js
+++ client/src/CoreComponents/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        account: {}
+        account: null
     },
     reducers: {
         displayUser: (state, action) => {
